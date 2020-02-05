58f04682d36849b8034b7325135c7041d204b7b2
PichlerMartin <40146720150314@litec.ac.at>
2020-02-05 11:35:51 +0100

    updated Program.cs

--- wifi-analyzer/Program.cs
+++ wifi-analyzer/Program.cs
@@ -1,10 +1,7 @@
 ﻿using NativeWifi;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static NativeWifi.Wlan;
 
 namespace wifi_analyzer
@@ -26,26 +23,82 @@ namespace wifi_analyzer
             foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
             {
                 Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
-                Wlan.WlanBssEntry[] bssidd = wlanIface.GetNetworkBssList();
+                Wlan.WlanBssEntry[] bssidds = wlanIface.GetNetworkBssList();
 
                 int i = 1;
                 
                 foreach(WlanAvailableNetwork network in networks)
                 {
+                    int x = 1;
+
                     if (!network.profileName.Equals(""))
                     {
-                        Console.WriteLine("SSID {0} : {1}", i, network.profileName);
-                        Console.WriteLine("Authentifizierung:       {0}", network.dot11DefaultAuthAlgorithm);
-                        Console.WriteLine("Verschlüsselung:         {0}", network.dot11DefaultCipherAlgorithm);
-                        Console.WriteLine("Netzwertyp:              {0}", network.dot11BssType);
-                        Console.WriteLine("");
+                        Console.WriteLine("SSID {0} :               {1}", i, network.profileName);
+                        Console.WriteLine("     Authentifizierung:       {0}", network.dot11DefaultAuthAlgorithm);
+                        Console.WriteLine("     Verschlüsselung:         {0}", network.dot11DefaultCipherAlgorithm);
+                        Console.WriteLine("     Netzwertyp:              {0}", network.dot11BssType);
+                    }
+
+                    foreach(Wlan.WlanBssEntry bssidd in bssidds)
+                    {
+                        if (bssidd.dot11Ssid.SSID.SequenceEqual(network.dot11Ssid.SSID) && !network.profileName.Equals(""))
+                        {
+                            Console.WriteLine("     BSSID {0}:          {1}", x, ConvertDecimalArrayToHexString(bssidd.dot11Bssid));
+
+                            Console.WriteLine("         Signal:                     {0}", bssidd.linkQuality);
+                            Console.WriteLine("         Frequenz:                   {0} GHz", bssidd.chCenterFrequency/1000000.0);
+                            Console.WriteLine("         Funktyp:                    {1}", x, ConvertDecimalArrayToHexString(bssidd.dot11Bssid));
+                            Console.WriteLine("         Kanal:                      {1}", x, ConvertDecimalArrayToHexString(bssidd.dot11Bssid));
+                            Console.WriteLine("         Basisrate (MBit/s):         {0}", wlanIface.Channel);
+                            Console.WriteLine("         Andere Raten (MBit/s):      {0}", getRatesFromBssid(bssidd.wlanRateSet));
+                            Console.WriteLine("         Andere Raten (MBit/s):      {0}", getAllRatesFromBssid(bssidd.wlanRateSet));
+                            x++;
+                        }
+
                     }
 
                     i++;
+                    Console.WriteLine("");
                 }
             }
 
             Console.ReadKey();
+        }
+
+        private static string getRatesFromBssid(WlanRateSet wlanRateSet)
+        {
+            string ret = "";
+            foreach (int i in wlanRateSet.Rates)
+            {
+                ret += i + " ";
+            }
+
+            return ret.Remove(ret.Length - 1);
+        }
+
+        private static string getAllRatesFromBssid(WlanRateSet wlanRateSet)
+        {
+            string ret = "";
+            for(int x = 0; x < 100; x++)
+            {
+                if(wlanRateSet.GetRateInMbps(x) != 0)
+                {
+                    ret += wlanRateSet.GetRateInMbps(x) + " ";
+                }
+            }
+
+            return ret.Remove(ret.Length - 1);
+        }
+
+        private static string ConvertDecimalArrayToHexString(byte[] ar)
+        {
+            String ret = "";
+            foreach(byte b in ar)
+            {
+                ret += b.ToString("X");
+                ret += ":";
+            }
+            return ret.Remove(ret.Length - 1);
         }
     }
 }
